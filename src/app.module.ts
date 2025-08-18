399d8cd40a3391184ffd94f581c2d3d007565be5
Bettina <bettike18989@gmail.com>
2025-08-18 21:15:20 +0200

    SignIn function

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,7 +15,7 @@ import { AuthModule } from './auth/auth.module';
       host: process.env.DB_HOST,
       port: parseInt(process.env.DB_PORT || '5433', 10),
       username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
+      password: process.env.DB_PASSWORD_NEW,
       database: process.env.DB_NAME,
       entities: [User],
 
@@ -27,3 +27,39 @@ import { AuthModule } from './auth/auth.module';
   ],
 })
 export class AppModule { }
+
+
+// import { Module } from '@nestjs/common';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+// import { ConfigModule, ConfigService } from '@nestjs/config';
+// import { UserModule } from './user/user.module';
+// import { AuthModule } from './auth/auth.module';
+
+// @Module({
+//   imports: [
+//     ConfigModule.forRoot({ isGlobal: true }),
+
+//     TypeOrmModule.forRootAsync({
+//       inject: [ConfigService],
+//       useFactory: (cfg: ConfigService) => ({
+//         type: 'postgres',
+//         host: cfg.get<string>('DB_HOST'),
+//         port: parseInt(cfg.get('DB_PORT') ?? '5433', 10),
+//         username: cfg.get<string>('DB_USERNAME'),
+//         password: cfg.get<string>('DB_PASSWORD_NEW'),
+//         database: cfg.get<string>('DB_NAME'),
+//         autoLoadEntities: true,       // зручніше за entities:[User]
+//         synchronize: false,           // true тільки в деві, на проді краще міграції
+//         ssl: process.env.NODE_ENV === 'production'
+//           ? { rejectUnauthorized: false }
+//           : false,
+//         // logging: true, // тимчасово для діагностики
+//       }),
+//     }),
+
+//     UserModule,
+//     AuthModule,
+//   ],
+// })
+// export class AppModule { }
+
