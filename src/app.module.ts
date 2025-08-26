6ebcaf8cecb327e26f88fcd008b06983376bf28d
Bettina <bettike18989@gmail.com>
2025-08-26 12:56:17 +0200

    dist

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,67 +1,68 @@
 // 1. app.module.ts signup, signin для locslhost 5000!!!
 
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
+// import { Module } from '@nestjs/common';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+// import { ConfigModule } from '@nestjs/config';
 
-import { User } from './user/user.entity';
-import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module'; 
+// import { User } from './user/user.entity';
+// import { UserModule } from './user/user.module';
+// import { AuthModule } from './auth/auth.module'; 
 
-@Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+// @Module({
+//   imports: [
+//     ConfigModule.forRoot({ isGlobal: true }),
 
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT || '5433', 10),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      entities: [User],
+//     TypeOrmModule.forRoot({
+//       type: 'postgres',
+//       host: process.env.DB_HOST,
+//       port: parseInt(process.env.DB_PORT || '5433', 10),
+//       username: process.env.DB_USERNAME,
+//       password: process.env.DB_PASSWORD,
+//       database: process.env.DB_NAME,
+//       entities: [User],
 
-      synchronize: true,
-    }),
+//       synchronize: true,
+//     }),
 
-    UserModule,
-    AuthModule, 
-  ],
-})
-export class AppModule { }
+//     UserModule,
+//     AuthModule, 
+//   ],
+// })
+// export class AppModule { }
+
 
 
 
 
 //2. app.module.ts signup, signin для продакшена!!!
 
-// import { Module } from '@nestjs/common';
-// import { TypeOrmModule } from '@nestjs/typeorm';
-// import { ConfigModule } from '@nestjs/config';
-// import { UserModule } from './user/user.module';
-// import { AuthModule } from './auth/auth.module';
-// import { TerminusModule } from '@nestjs/terminus';
-// import { HealthController } from './health/health.controller';
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { TerminusModule } from '@nestjs/terminus';
+import { HealthController } from './health/health.controller';
 
-// const isExternal = (process.env.DATABASE_URL ?? '').includes('.internal') === false;
+const isExternal = (process.env.DATABASE_URL ?? '').includes('.internal') === false;
 
-// @Module({
-//   imports: [
-//     ConfigModule.forRoot({ isGlobal: true }),
-//     TypeOrmModule.forRoot({
-//       type: 'postgres',
-//       url: process.env.DATABASE_URL,
-//       autoLoadEntities: true,
-//       synchronize: false, // <- фіксуємо вимкнено
-//       ssl: isExternal ? { rejectUnauthorized: false } : false,
-//     }),
-//     TerminusModule,
-//     UserModule,
-//     AuthModule,
-//   ],
-//   controllers: [HealthController],
-// })
-// export class AppModule { }
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      autoLoadEntities: true,
+      synchronize: false, // <- фіксуємо вимкнено
+      ssl: isExternal ? { rejectUnauthorized: false } : false,
+    }),
+    TerminusModule,
+    UserModule,
+    AuthModule,
+  ],
+  controllers: [HealthController],
+})
+export class AppModule { }
 
 
 
