965ecf441016a915c9dd49f341c7d65a648149bb
Bettina <bettike18989@gmail.com>
2025-09-01 16:05:41 +0200

    admin

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,71 +1,3 @@
-// // 1. app.module.ts signup, signin для locslhost 5000!!!
-
-// import { Module } from '@nestjs/common';
-// import { TypeOrmModule } from '@nestjs/typeorm';
-// import { ConfigModule } from '@nestjs/config';
-
-// import { User } from './user/user.entity';
-// import { UserModule } from './user/user.module';
-// import { AuthModule } from './auth/auth.module'; 
-
-// @Module({
-//   imports: [
-//     ConfigModule.forRoot({ isGlobal: true }),
-
-//     TypeOrmModule.forRoot({
-//       type: 'postgres',
-//       host: process.env.DB_HOST,
-//       port: parseInt(process.env.DB_PORT || '5433', 10),
-//       username: process.env.DB_USERNAME,
-//       password: process.env.DB_PASSWORD,
-//       database: process.env.DB_NAME,
-//       entities: [User],
-
-//       synchronize: true,
-//     }),
-
-//     UserModule,
-//     AuthModule, 
-//   ],
-// })
-// export class AppModule { }
-
-
-
-
-
-// //2. app.module.ts signup, signin для продакшена!!!
-
-// // import { Module } from '@nestjs/common';
-// // import { TypeOrmModule } from '@nestjs/typeorm';
-// // import { ConfigModule } from '@nestjs/config';
-// // import { UserModule } from './user/user.module';
-// // import { AuthModule } from './auth/auth.module';
-// // import { TerminusModule } from '@nestjs/terminus';
-// // import { HealthController } from './health/health.controller';
-
-// // const isExternal = (process.env.DATABASE_URL ?? '').includes('.internal') === false;
-
-// // @Module({
-// //   imports: [
-// //     ConfigModule.forRoot({ isGlobal: true }),
-// //     TypeOrmModule.forRoot({
-// //       type: 'postgres',
-// //       url: process.env.DATABASE_URL,
-// //       autoLoadEntities: true,
-// //       synchronize: false, // <- фіксуємо вимкнено
-// //       ssl: isExternal ? { rejectUnauthorized: false } : false,
-// //     }),
-// //     TerminusModule,
-// //     UserModule,
-// //     AuthModule,
-// //   ],
-// //   controllers: [HealthController],
-// // })
-// // export class AppModule { }
-
-
-
 // src/app.module.ts
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -120,7 +52,7 @@ import { HealthController } from './health/health.controller';
           autoLoadEntities: true,
           synchronize: false,                            // ⛔️
           migrationsRun: true,                           // ✅
-          migrations: [join(__dirname, 'migrations/*.{js,ts}')],
+          migrations: [join(__dirname, 'migrations/*.{ts,js}')],
           ssl: needSsl ? { rejectUnauthorized: false } : false,
         };
       },
