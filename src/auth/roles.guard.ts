e241cbd87b210e87dc954cf13f5de4312b05261f
Bettina <bettike18989@gmail.com>
2025-09-11 10:47:47 +0200

    Cookie-consent

--- src/auth/roles.guard.ts
+++ src/auth/roles.guard.ts
@@ -1,32 +1,3 @@
-
-// import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-// import { Reflector } from '@nestjs/core';
-// import { Observable } from 'rxjs';
-// import { UserPayload } from '../types/user-payload';
-
-
-
-// @Injectable()
-// export class RolesGuard implements CanActivate {
-//     constructor(private reflector: Reflector) { }
-
-//     canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
-//         const requiredRoles = this.reflector.getAllAndMerge<string[]>('roles', [
-//             context.getHandler(),
-//             context.getClass(),
-//         ]);
-//         if (!requiredRoles.length) return true;
-
-//         const req = context.switchToHttp().getRequest();
-//         const user = req.user as UserPayload;
-//         if (!user?.role) return false;
-//         return requiredRoles.includes(user.role);
-
-//     }
-// }
-
-
-
 import { CanActivate, ExecutionContext, ForbiddenException, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { ROLES_KEY, AppRole } from './roles.decorator';
